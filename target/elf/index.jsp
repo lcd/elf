9d00f7e297a7dad024991602cc0a7bf466ba5005
laichendong <laichendong@126.com>
2011-04-16 23:45:37 +0800

    delete build result files.

--- target/elf/index.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<h2>Hello World!asda</h2>
-</body>
-</html>
--- target/elf/js/tiny_mce/plugins/paste/langs/zh_dlg.js
+++
@@ -1,5 +0,0 @@
-tinyMCE.addI18n('zh.paste_dlg',{
-text_title:"\u7528 Ctrl+V \u5C06\u5185\u5BB9\u8D34\u4E0A\u3002",
-text_linebreaks:"\u4FDD\u7559\u5206\u884C\u7B26\u53F7",
-word_title:"\u7528 Ctrl+V \u5C06\u5185\u5BB9\u8D34\u4E0A\u3002"
-});
