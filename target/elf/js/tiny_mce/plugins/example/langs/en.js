d863405f219893c078b8cc22cb6041efee90996c
laichendong <laichendong@126.com>
2011-05-20 11:02:06 +0800

    delete target/*

--- target/elf/js/tiny_mce/plugins/example/langs/en.js
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-tinyMCE.addI18n('en.example',{
-	desc : 'This is just a template button'
-});
-=======
-tinyMCE.addI18n('en.example',{
-	desc : 'This is just a template button'
-});
->>>>>>> c5dd20a1ddcf3a1242761d65546b427a783df6c6
